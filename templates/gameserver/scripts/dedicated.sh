b7a59f4dfc0843d033aed25980ea5c8fb6af10b7
filipopo <filipmymail@gmail.com>
2025-01-14 01:25:09 +0100

    Start to make website look better

--- templates/gameserver/scripts/dedicated.sh
+++ templates/gameserver/scripts/dedicated.sh
@@ -3,6 +3,6 @@
 rm -f server.log
 command='wine UCC.exe server mf-Ghetto?game=RageGame.RageSafeCracker?mutator=vote.vote,voteMutWindow.windowMut ini=MobileForces.ini log=server.log'
 until $command; do
-  echo "Server crashed with exit code $?, restarting!" > &2
+  echo "Server crashed with exit code $?, restarting!" >&2
   sleep 1
 done
