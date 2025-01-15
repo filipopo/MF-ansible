1f34ae19f986d4a20d485ae954b6f6565e50ffe1
filipopo <filipmymail@gmail.com>
2025-01-15 23:28:54 +0100

    Update guide height

--- templates/webserver/website/public/js/guide_close.js
+++ templates/webserver/website/public/js/guide_close.js
@@ -1,9 +1,15 @@
 // @license magnet:?xt=urn:btih:b8999bbaf509c08d127678643c515b9ab0836bae&dn=ISC.txt ISC
 const el = document.getElementById('close');
+const iframe = document.getElementsByName('guide')[0];
 
 el.style.display = 'none';
-el.firstChild.addEventListener('click', () => el.style.display = 'none');
+el.addEventListener('click', () => el.style.display = 'none');
 
-for (let link of document.querySelectorAll('li > a'))
+for (let link of document.getElementsByName('open'))
   link.addEventListener('click', () => el.style.display = 'initial');
+
+iframe.addEventListener('load', () => {
+  const iframeContent = iframe.contentWindow.document;
+  iframe.style.height = iframeContent.documentElement.scrollHeight + 'px';
+});
 // @license-end
