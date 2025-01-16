f7ecc7fbf633d8489e6cac5852ec20feb79005be
filipopo <filipmymail@gmail.com>
2025-01-16 21:10:28 +0100

    Add crazy slideshow

--- templates/webserver/website/public/js/guide_close.js
+++ templates/webserver/website/public/js/guide_close.js
@@ -1,13 +1,12 @@
 // @license magnet:?xt=urn:btih:b8999bbaf509c08d127678643c515b9ab0836bae&dn=ISC.txt ISC
 const el = document.getElementById('close');
-const iframe = document.getElementsByName('guide')[0];
-
 el.style.display = 'none';
 el.addEventListener('click', () => el.style.display = 'none');
 
 for (let link of document.getElementsByName('open'))
   link.addEventListener('click', () => el.style.display = 'initial');
 
+const iframe = document.getElementsByName('guide')[0];
 iframe.addEventListener('load', () => {
   const iframeContent = iframe.contentWindow.document;
   iframe.style.height = iframeContent.documentElement.scrollHeight + 'px';
