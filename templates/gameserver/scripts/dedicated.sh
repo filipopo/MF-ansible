80824505d8bb0a5605fb333a82637c743f1ae1ef
Gregaras <69623172+Gregaras@users.noreply.github.com>
2025-04-11 01:44:33 +0300

    Update dedicated.sh
    Forgot one mutator, added it in

--- templates/gameserver/scripts/dedicated.sh
+++ templates/gameserver/scripts/dedicated.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f server.log
-command='wine UCC.exe server mf-Ghetto?game=RageGame.RageSafeCracker?mutator=Vote.Vote,VoteMutWindow.WindowMut,MutPack.AddWeaps,MutPack.ChangeBotInv,MutPack.ReplaceVehicles,MutPack.AddVehicles,MutPack.Commands,MutPack.ChangeDefProperties,MutPack.ChangeDamage ini=MobileForces.ini log=server.log'
+command='wine UCC.exe server mf-Ghetto?game=RageGame.RageSafeCracker?mutator=Vote.Vote,VoteMutWindow.WindowMut,VoteHUD.VoteHUD,MutPack.AddWeaps,MutPack.ChangeBotInv,MutPack.ReplaceVehicles,MutPack.AddVehicles,MutPack.Commands,MutPack.ChangeDefProperties,MutPack.ChangeDamage ini=MobileForces.ini log=server.log'
 until $command; do
   echo "Server crashed with exit code $?, restarting!" >&2
   sleep 1
