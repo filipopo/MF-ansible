c291fe9d2b92c23fcef6bb337814cb1f8d32f6e9
filipopo <filipmymail@gmail.com>
2025-04-11 01:09:46 +0200

    Randomise map

--- templates/gameserver/scripts/dedicated.sh
+++ templates/gameserver/scripts/dedicated.sh
@@ -1,7 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+map=(
+  'mf-airport'
+  'mf-Carpark'
+  'mf-dockyard'
+  'mf-Ghetto'
+  'mf-Hydroworks'
+  'mf-Polar'
+  'mf-Rail_Quarry'
+  'mf-sawmill'
+  'mf-warehouse'
+  'mf-waterfront'
+  'mf-western'
+)
+
+mode=(
+  'RageGame.RageSafeCracker'
+)
+
+index=$(( RANDOM % ${#map[@]} ))
+map="${map[$index]}"
+
+index=$(( RANDOM % ${#mode[@]} ))
+mode="${mode[$index]}"
 
 rm -f server.log
-command='wine UCC.exe server mf-Ghetto?game=RageGame.RageSafeCracker?mutator=Vote.Vote,VoteMutWindow.WindowMut,VoteHUD.VoteHUD,MutPack.AddWeaps,MutPack.ChangeBotInv,MutPack.ReplaceVehicles,MutPack.AddVehicles,MutPack.Commands,MutPack.ChangeDefProperties,MutPack.ChangeDamage ini=MobileForces.ini log=server.log'
+command="wine UCC.exe server \
+  $map?game=$mode?mutator=Vote.Vote,VoteMutWindow.WindowMut,VoteHUD.VoteHUD,MutPack.AddWeaps,MutPack.ChangeBotInv,MutPack.ReplaceVehicles,MutPack.AddVehicles,MutPack.Commands,MutPack.ChangeDefProperties,MutPack.ChangeDamage \
+  ini=MobileForces.ini log=server.log"
+
 until $command; do
   echo "Server crashed with exit code $?, restarting!" >&2
   sleep 1
